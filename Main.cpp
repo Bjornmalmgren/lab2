8dce031c9ac2f178456292efef1a1e8404820650
Monicaco1988 <edmin.aguilaroddershede@gmail.com>
2024-04-18 15:44:44 +0200

    stack funkar med mellanslag

--- Main.cpp
+++ Main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <stack>
+#include <queue>
 using namespace std;
 
 bool IsPalindromStacksOnly(string input) {
 	stack<char> stack;
 	bool isPalindrome = true;
-	char space[2] = " ";
+
+	//removed redundante
+	//char space[2] = " ";
+
+	//function below removes spaces in the "input"
+	input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
 	for (size_t i = 0; i < input.length(); i++)
 	{
 		stack.push(input[i]);
 	}
 	for (size_t i = 0; i < input.length(); i++)
 	{
-
-		if (stack.top() == space[0]) {
-			stack.pop();
-		}
-		if (input[i] == space[0])
-		{
-			i++;
-		}
+		// removed this below, redundante
+		//if (stack.top() == space[0]) {
+		//	stack.pop();
+		//}
+		//if (input[i] == space[0])
+		//{
+		//	i++;
+		//}
 		if (input[i] != stack.top()) {
 			isPalindrome = false;
 			break;
@@ -33,8 +39,19 @@ bool IsPalindromStacksOnly(string input) {
 	return isPalindrome;
 }
 
+//
+//bool IsAPalindromQueueAndStack(string input)
+//{
+//	queue<string> queue;
+//	stack<string> stack;
+//
+//	bool isPalindrome = true;
+//
+//
+//}
+
 int main() {
-	string input = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	string input = " afaf";
 	bool palindrome = false;
 	palindrome = IsPalindromStacksOnly(input);
 
@@ -45,9 +62,5 @@ int main() {
 	{
 		cout << "is not palindrome";
 	}
-
-
-
-
 	return 0;
 }
