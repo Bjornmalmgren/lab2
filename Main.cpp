d088457680569d91b544c61e09f12c9a112d61e2
Bjornmalmgren <frassebjorn@gmail.com>
2024-04-30 14:39:19 +0200

    new version

--- Main.cpp
+++ Main.cpp
@@ -1,109 +1,50 @@
 #include <iostream>
-#include <stack>
-#include <queue>
-#include <windows.h>
-#include <chrono>
-#include <string>
+#include <vector>
 
 using namespace std;
 
-bool IsPalindromStacksOnly(string input) {
-	stack<char> stack;
-	bool isPalindrome = true;
-	//function below removes spaces in the "input"
-	input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
-	for (size_t i = 0; i < input.length(); i++)
-	{
-		stack.push(input[i]);
-	}
-	for (size_t i = 0; i < input.length(); i++)
-	{
-		if (input[i] != stack.top()) {
-			isPalindrome = false;
-			break;
-		}
-		else
-		{
-			stack.pop();
-		}
 
-	}
-	return isPalindrome;
-}
-
-
-
-string IsAPalindromQueueAndStack(string input)
+//vector <int> Compare(vector<int>& elements, vector<int>& left, vector<int>& right)
+vector <int> Compare(vector<int>& left, vector<int>& right)
 {
-	queue<char> queue;
-	stack<char> stack;
-	//function below removes spaces in the "input"
-	input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
+    int totalSum = left[0] + right[0];
+    int maxPrefix = max(left[1], (left[0] + right[1]));
+    int maxSuffix = max(right[2], right[0] + left[2]);
+    int maxSum = max(max(left[3], right[3]), (left[2] + right[1]));
 
-	for (size_t i = 0; i < input.length() / 2; i++)
-	{
-		stack.push(input[i]);
-	}
-	if (input.length() % 2 == 1) // removes the middle char
-	{
-		for (size_t i = (1 + input.length() / 2); i < input.length(); i++) // skips the middle char
-		{
-			queue.push(input[i]);
-		}
-	}
-	else
-	{
-		for (size_t i = (input.length() / 2); i <= input.length() - 1; i++) // other half of input string
-		{
-			queue.push(input[i]);
-		}
-	}
-	for (size_t i = 0; i < stack.size(); i++)
-	{
-		if (queue.front() != stack.top())
-		{
-			return "is not a palindrome";
-		}
-		queue.pop();
-		stack.pop();
-	}
-	return "is a palindrome";
+    return { totalSum, maxPrefix, maxSuffix, maxSum };
 }
 
-int main() {
-	int amountOfCharMultiplier = 1;
-	for (int amountOfCharMultiplier = 1; amountOfCharMultiplier < 6; amountOfCharMultiplier++)
-	{
-		string input = "";
-		for (int j = 0; j < 1000000 * amountOfCharMultiplier; j++)
-		{
-			input += "af";
-		}
-		bool palindrome = false;
-		cout << "Amout of characters that are in the palindrom: " << input.length();
-		auto start1 = chrono::steady_clock::now();
-		palindrome = IsPalindromStacksOnly(input);
-		auto end1 = chrono::steady_clock::now();
-		cout << "Stack time: \n"
-			<< chrono::duration_cast<chrono::milliseconds>(end1 - start1).count()
-			<< "ms" << endl;
-		if (palindrome) {
-			cout << "is a palindrome";
-		}
-		else
-		{
-			cout << "is not a palindrome";
-		}
-		cout << endl;
-		auto start = chrono::steady_clock::now();
-		IsAPalindromQueueAndStack(input);
-		auto end = chrono::steady_clock::now();
-		IsAPalindromQueueAndStack(input);
-		cout << "Queue/stack time: \n"
-			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
-			<< "ms" << endl;
-	}
-	return 0;
+vector<int> FunctionMaxSubarray(vector<int>& elements, int low, int high)
+{
+    if (low == high)
+    {
+        return { elements[low], elements[low], elements[low], elements[low] };
+    }
+    else
+    {
+        int mid = (low + high) / 2;
+        vector <int> left = FunctionMaxSubarray(elements, low, mid);
+        vector <int> right = FunctionMaxSubarray(elements, mid + 1, high);
+        return Compare(left, right); // old return Compare(A, left, right)
+    }
+
 }
 
-// Conclusion, seems like the stack is faster with few number of digits when compareing palindromes but worse with larger numbers
+
+int main()
+{
+    vector<int> elements = { -1,-2,-3,-4,-5 };
+
+    int size = elements.size(); // calculate the entire sum of the elements
+
+    vector<int> max_sum = FunctionMaxSubarray(elements, 0, size - 1); // function to find subarray
+
+
+    cout << "tot sum :" << max_sum[0] << endl;
+    cout << "max prefix:" << max_sum[1] << endl;
+    cout << "mx suffix:" << max_sum[2] << endl;
+    cout << "max sum of consecutive array:" << max_sum[3] << endl;
+
+    return 0;
+}
